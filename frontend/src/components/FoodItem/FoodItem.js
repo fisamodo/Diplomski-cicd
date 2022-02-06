4566b2d246d8547fd5e79ec2422625015d171dbd
Filip Velebit Samodol <fisamodo@vsite.hr>
2022-02-06 22:05:42 +0100

    Fix commit

--- frontend/src/components/FoodItem/FoodItem.js
+++ frontend/src/components/FoodItem/FoodItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './../FoodContainer/FoodContainer.css'
 const FoodItem = ({ food }) => {
+  let item = "SUssy"
   return <div className='foodItem'>
     <span>{food[1].name}</span><br />
     <span>{food[1].calories}</span>
-    <span>Sossy adad</span>
+    <span>Sossy adad tests</span>
   </div>;
 };
 
