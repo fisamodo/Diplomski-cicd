aaf5ac15558411f6c4548d5470fc2e52878ebb0f
Filip Velebit Samodol <fisamodo@vsite.hr>
2022-02-06 23:59:22 +0100

    Pipe

--- frontend/src/components/FoodItem/FoodItem.js
+++ frontend/src/components/FoodItem/FoodItem.js
@@ -5,7 +5,7 @@ const FoodItem = ({ food }) => {
     <span>{food[1].name}</span><br />
     <span>{food[1].calories}</span>
     <span>Sossy</span>
-    <span>Test pipeline develop merge</span>
+    <span>Test pipeline 11:58</span>
   </div>;
 };
 
