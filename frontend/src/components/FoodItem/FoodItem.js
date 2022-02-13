9eef1cb8ffd4b750b0eeec908ea484669d64d741
Filip Velebit Samodol <fisamodo@vsite.hr>
2022-02-13 11:42:16 +0100

    Linter

--- frontend/src/components/FoodItem/FoodItem.js
+++ frontend/src/components/FoodItem/FoodItem.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './../FoodContainer/FoodContainer.css'
+import React from "react";
+import "./../FoodContainer/FoodContainer.css";
 const FoodItem = ({ food }) => {
-  return <div className='foodItem'>
-     <span>{food[1].name}</span><br />
+  return (
+    <div className="foodItem">
+      <span>{food[1].name}</span>
+      <br />
       <span>{food[1].calories}</span>
-          <span>Sossy</span>
-    <span>Test pipeline 12:43</span>
-  </div>;
+      <span>Sossy</span>
+
+      <span>Test pipeline 12:43</span>
+    </div>
+  );
 };
 
 export default FoodItem;
